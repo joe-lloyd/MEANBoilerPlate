d07dd1da5a1ca377f42aee11f734d75b36356bb2
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2016-03-29 21:45:15 +0200

    changed the word params to data to pass the password and username and it worked

--- frontend/app/modules/home/home-service.js
+++ frontend/app/modules/home/home-service.js
@@ -2,11 +2,24 @@ angular.module('storeHomeService', []).factory('homeFactory', [ '$http',  functi
 	return{
 		getStuff: function(){
 			console.log('users hit');
-			return $http.get('http://localhost:3000/api/users');
+
+			return $http({
+				method: 'GET',
+				url: 'http://localhost:3000/api/users'
+			});
+
 		},
 		logMyAssIn: function(username, password){
 			console.log('trying to login');
-			return $http.post('http://localhost:3000/api/login', {params : {username : username, password: password}});
+
+			return $http({
+				method: 'POST',
+				url: 'http://localhost:3000/api/login',
+				data: {
+					'password': password,
+					'username': username
+				}
+			});
 		}
 	};
 }]);

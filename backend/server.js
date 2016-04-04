d606adbfc7c8f03f78bc42f38ee8e67e9a6f9e6a
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2016-04-04 21:35:00 +0200

    cleanup server js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
-// server.js
-
 // BASE SETUP
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var mongoose = require('mongoose');
-var app        = express();                 // define our app using express
-var path = require('path');
+var express 	= require('express'); 	// call express
+var app			= express(); 			// define our app using express
+var path 		= require('path');		// get path to help resolve concat urls
 
-var conf = require('./modules/serverConf.js')(app, express, __dirname);
-var user = require('./models/user.js');
-var auth = require('./modules/auth.js')(app, user);
-var routes = require('./routes/index');
+// custom modules and modles defined
+var conf 	= require('./modules/serverConf.js')(app, express, __dirname);
+var user 	= require('./models/user.js');
+var auth 	= require('./modules/auth.js')(app, user);
+var routes 	= require('./routes/index');
 
 app.use('/api', routes);
 
-var db = require('./modules/mongodb.js');		// getting the connectionto the database via a self made module
-
+// database connection via a custom module
+var db = require('./modules/mongodb.js');	// getting the connectionto the database via a self made module
 
 // this comes last so as not to mess with our api
 app.all('/*', function(req, res) {
@@ -26,4 +24,7 @@ app.all('/*', function(req, res) {
 	
 });
 
-app.listen(3000);
+// run app from port 3000
+app.listen(3000, function(){
+	console.log('connected on port 3000');
+});

c03068e3501cfe3d9c9d26b6432623e2bdb86d77
Joseph M. Lloyd <joe.lloyd.22.24@gmail.com>
2016-04-06 22:08:18 +0200

    added chained routes also added a get one route to return an object not an array

--- backend/routes/page.js
+++ backend/routes/page.js
@@ -2,63 +2,75 @@ var express = require('express');
 var page = require('../models/page');
 var router = express.Router();
 
+router.route('/page')
 
-router.post('/page',function(req, res) { 
-	var newPage = new page({
-		title: req.body.title,
-		text: req.body.text,
-		excerpt: req.body.excerpt,
-		imageUrl: req.body.imgUrl
+	.post( function(req, res) { 
+		var newPage = new page({
+			title: req.body.title,
+			text: req.body.text,
+			excerpt: req.body.excerpt,
+			imageUrl: req.body.imgUrl
+			});
+
+		newPage.save(function(err){
+			if (err) return console.log(err);
+
+			res.json({saved: true});
+		});
+	})
+
+
+	.get( function(req, res) {
+
+		page.find({}, function(err, page){
+			res.json(page);
 		});
 
-	newPage.save(function(err){
-		if (err) return console.log(err);
-
-		res.json({saved: true});
-	});
-});
-
-
-router.get('/page',function(req, res) {
-
-	page.find({}, function(err, page){
-		res.json(page);
 	});
 
-});
+router.route('/page/:id')
+
+	.delete( function(req, res) { 
+
+		var page_id = {_id: req.params.id};
+
+		page.findOneAndRemove(page_id, function(err, page){
+			if (err) return console.log(err);
+
+			res.json({deleted: true});
+		});
+
+	})
 
 
-router.delete('/page/:id',function(req, res) { 
+	.put( function(req, res) { 
 
-	var page_id = {_id: req.params.id};
+		var page_id = {_id: req.params.id};
 
-	page.findOneAndRemove(page_id, function(err, page){
-		if (err) return console.log(err);
+		var updatePage = {$set: {
+			title: req.body.title,
+			text: req.body.text,
+			excerpt: req.body.excerpt,
+			imageUrl: req.body.imgUrl
+			}};
 
-		res.json({deleted: true});
+
+		page.findOneAndUpdate(page_id, updatePage, function(err, page){
+			if (err) return console.log(err);
+
+			res.json({updated: true});
+		});
+
+	})
+
+	.get( function(req, res) {
+
+		var page_id = {_id: req.params.id};
+
+		page.findOne({_id: page_id}, function(err, page){
+			res.json(page);
+		});
+
 	});
-
-});
-
-
-router.put('/page/:id',function(req, res) { 
-
-	var page_id = {_id: req.params.id};
-
-	var updatePage = {$set: {
-		title: req.body.title,
-		text: req.body.text,
-		excerpt: req.body.excerpt,
-		imageUrl: req.body.imgUrl
-		}};
-
-
-	page.findOneAndUpdate(page_id, updatePage, function(err, page){
-		if (err) return console.log(err);
-
-		res.json({updated: true});
-	});
-
-});
 
 module.exports = router;
